89cb8e267ec925e256d18cbe83e7f199deadd0aa
Manish-621 <manishkmurali621@gmail.com>
2023-02-03 12:56:14 +0530

    initial

--- lms/jstools/tailwind.config.js
+++ lms/jstools/tailwind.config.js
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  important: true,
+  future: {
+      removeDeprecatedGapUtilities: true,
+      purgeLayersByDefault: true,
   },
+  purge: {
+      enabled: false, //true for production build
+      content: [
+          '../**/templates/*.html',
+          '../**/templates/**/*.html'
+      ]
+  },
+  theme: {
+      extend: {},
+  },
+  variants: {},
   plugins: [],
 }
